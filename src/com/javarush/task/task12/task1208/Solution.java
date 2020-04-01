bd6562b8f081774a5d100d9c7eb5d82df92a9160
Sergey Demidov <ssevod@gmail.com>
2020-04-01 13:01:08 +0300

    12.04

---
+++ src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    //Напишите тут ваши методы
+    public static void print (String s){}
+    public static void print (int s){}
+    public static void print (Integer s){}
+    public static void print (char s){}
+    public static void print (long s){}
+
+}
---
+++ src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int min(int i, int j){
+        if (i < j)
+            return i;
+        else
+            return j;
+    }
+    public static long min(long i, long j){
+        if (i < j)
+            return i;
+        else
+            return j;
+    }
+    public static double min(double i, double j){
+        if (i < j)
+            return i;
+        else
+            return j;
+    }
+}
